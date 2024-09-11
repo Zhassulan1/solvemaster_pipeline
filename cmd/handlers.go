a3275c3d8bdbdf865567940f26497f39740a8dc2
Zhassulan Kainazarov <zhkainazarov@gmail.com>
2024-09-11 08:21:15 +0000

    feat: returns detailed output/error in json format

--- cmd/handlers.go
+++ cmd/handlers.go
@@ -11,15 +11,40 @@ func HealthCheck(w http.ResponseWriter, r *http.Request) {
 }
 
 func Update(w http.ResponseWriter, r *http.Request) {
-	cmd := exec.Command("sh", "./update.sh")
+	pullCMD := exec.Command("git", "-C", "/home/ubuntu/solvemaster_pipeline/", "pull")
+	supervisorCMD := exec.Command("supervisorctl", "restart", "solvemaster_pipeline")
 
-	_, err := cmd.CombinedOutput()
-
-	if err != nil {
+	pullOutput, pullERR := pullCMD.CombinedOutput()
+	if pullERR != nil {
 		// if there was any error, print it here
-		w.Write([]byte(err.Error()))
+		err := writeJSON(w, 500, envelope{
+			"git_pull_err":    pullERR,
+			"git_pull_output": pullOutput,
+		}, nil)
+		if err != nil {
+			w.WriteHeader(500)
+		}
+		return
 	}
-	w.Write([]byte("OK"))
-	// otherwise, print the output from running the command
-	// fmt.Println("Output: ", string(out))
+
+	supervisorOutput, supervisorERR := supervisorCMD.CombinedOutput()
+	if supervisorERR != nil {
+		// if there was any error, print it here
+		err := writeJSON(w, 500, envelope{
+			"git_pull_output":   pullOutput,
+			"supervisor_error":  supervisorERR,
+			"supervisor_output": supervisorOutput,
+		}, nil)
+
+		if err != nil {
+			w.WriteHeader(500)
+		}
+		return
+	}
+
+	writeJSON(w, 200, envelope{
+		"git_pull_output":   pullOutput,
+		"supervisor_output": supervisorOutput,
+	}, nil)
+	return
 }
--- update.sh
+++
@@ -1,4 +0,0 @@
-cd ~/solvmaster_backend
-pkill uvicorn
-git pull
-/home/ubuntu/env/bin/python3 /home/ubuntu/env/bin/uvicorn backend.main:app --reload > log.txt & disown
